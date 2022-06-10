a7a41be7bbf7360d7023e291e16974ff57d1e3e0
Matt Fantuzzi <53240059+FantuzziMatteo97@users.noreply.github.com>
2022-06-10 11:16:14 +0200

    added styling

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -6,6 +6,8 @@ from .models import *
 
 class TaskForm(forms.ModelForm):
 
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add New Task...'}))
+
     class Meta:
         model = Task
         fields = '__all__'
