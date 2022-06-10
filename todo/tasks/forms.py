9b71ae09ac825b1056d4fa75c5980dff1bd3feb7
Matt Fantuzzi <53240059+FantuzziMatteo97@users.noreply.github.com>
2022-06-10 10:33:16 +0200

    Project Init

---
+++ todo/tasks/forms.py
@@ -0,0 +1,11 @@
+import imp
+from django import forms
+from django.forms import ModelForm
+
+from .models import *
+
+class TaskForm(forms.ModelForm):
+
+    class Meta:
+        model = Task
+        fields = '__all__'
